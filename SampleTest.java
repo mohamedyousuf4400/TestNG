2f65cb0a63dabef9d561f72b3db2abc6f5f5ad66
Mohamed Yousuf <73808422+mohamedyousuf4400@users.noreply.github.com>
2021-12-22 10:03:50 +0530

    Create SampleTest.java
    Initial commit

---
+++ SampleTest.java
@@ -0,0 +1,27 @@
+package basicInTestNG;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {	
+
+	@Test
+	public void firstTestCase() {
+		System.out.println("This is my First Test Case"); 
+	}
+
+	@Test
+	public void secondTestCase() {
+		System.out.println("This is my Second Test Case"); 
+	}
+
+	@Test
+	public void thirdTestCase() {
+		System.out.println("This is my Third Test Case"); 
+	}
+
+	@Test
+	public void fourthTestCase() {
+		System.out.println("This is my Fourth Test Case"); 
+	}
+}
+
